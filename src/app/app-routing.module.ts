babeede7025aba0f3c5b7996cbcfddb685dd22da
SubathraSekar1 <suji50631@gmail.com>
2024-03-21 17:16:23 +0530

    Today update the  changes popup message and change the background images and apply for the media query

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {path:'signup',component:SignupComponent},
+  { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
    selector: 'app-signup',
@@ -9,8 +9,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
    styleUrl: './signup.component.css'
 })
 export class SignupComponent implements OnInit {
+   imageUrls: string[] = [
+      '/assets/images/img1.png',
+      '/assets/images/img2.png',
+      '/assets/images/img3.png',
+      '/assets/images/img4.jpg'
+   ];
+   currentImageUrlIndex = 0;
+   changeBackgroundImage() {
+      this.currentImageUrlIndex = (this.currentImageUrlIndex + 1) % this.imageUrls.length;
+   }
    signupForm: FormGroup | any;
-   constructor(private formBuilder: FormBuilder) { }
+   constructor(private formBuilder: FormBuilder, private toastr: ToastrService) { }
+   signup2() {
+      this.toastr.success("Your successfull signup!")
+   }
    ngOnInit() {
       this.signupForm = this.formBuilder.group({
          firstName: ['', Validators.required],
@@ -20,10 +33,17 @@ export class SignupComponent implements OnInit {
          confirmPassword: ['', [Validators.required]],
          mobileNumber: ['', Validators.required]
       });
+      this.startBackgroundImageLoop();
    }
-   submitForm() {
+   startBackgroundImageLoop(): void {
+      setTimeout(() => {
+         this.currentImageUrlIndex = (this.currentImageUrlIndex + 1) % this.imageUrls.length;
+         this.startBackgroundImageLoop();
+      }, 3000);
+   }
+   submitForm(signup01: FormGroup) {
+      console.log(signup01.value)
       this.signupForm.valid
       this.signupForm.reset();
-      return this.signupForm.value;
    }
 }
