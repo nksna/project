3724b18fe89ba83f966831ce34ba91a3f82ae7e7
SubathraSekar1 <suji50631@gmail.com>
2024-03-27 17:45:32 +0530

    I made the changes menu bar and apply the css style

--- src/app/layout/section/section.component.ts
+++ src/app/layout/section/section.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })
-export class SectionComponent {
-
+export class SectionComponent{
+  
 }
