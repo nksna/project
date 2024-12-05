ce061fe0b533fdd6378fb002410e94dbdc1175b9
Gayathri2023 <gayatri.s@fxprosinc.com>
2024-12-05 10:10:27 +0530

    set default display screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [  {path:'',component:HomeComponent},
+const routes: Routes = [  {path:'',component:LayoutComponent},
   { path: 'layout', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }];
-
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
