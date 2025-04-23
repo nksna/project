88c044b038abe0064b5e650f79b1a7305b57a8cc
yogeshwaran1820 <yogeshwaran1820@gmail.com>
2025-04-23 11:00:27 +0530

    basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [  {path:'',component:HomeComponent},
+const routes: Routes = [  {path:'',component:LoginComponent},
   { path: 'layout', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }];
 
 @NgModule({
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
-const routes: Routes = [{ path: '', component: LayoutComponent }];
+const routes: Routes = [
+  { path: '', component: LayoutComponent,
+    children:[
+      {path:'course',loadChildren:()=> import('../../app/component/course/course.module').then((m)=>m.CourseModule)}
+    ]
+   },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
